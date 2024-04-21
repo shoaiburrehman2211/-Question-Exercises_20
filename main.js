0075da365e7f6f97c9ca8f274be02b22cee7b353
Shoaib-ur-Rehman <160622745+shoaiburrehman2211@users.noreply.github.com>
2024-04-22 03:39:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+/*
+Think of something you could store in an array. For example, you could make a list of mountains,
+rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list
+containing these items.
+*/
+var progLan = ["Typescript", "Python", "Javascript", "Ruby", "C++"];
+// making list of these items
+progLan.forEach(function (item) {
+    console.log(item);
+});
---
+++ main.ts
@@ -0,0 +1,14 @@
+/*
+Think of something you could store in an array. For example, you could make a list of mountains, 
+rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list 
+containing these items.
+*/
+
+
+let progLan: string [] = ["Typescript", "Python", "Javascript", "Ruby", "C++"];
+
+// making list of these items
+
+progLan.forEach((item)=>{
+    console.log(item);
+})
